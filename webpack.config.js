43d385040d8a61d494940628496a63c591a88264
mortis133 <kryst.madry@gmail.com>
2023-10-14 16:27:08 +0200

    Add remote site

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const { dependencies } = require("./package.json");
+
+module.exports = {
+    entry: "./src/entry.js",
+    mode: "development",
+    devServer: {
+        port: 3002,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    },
+                    },
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+        new ModuleFederationPlugin({
+            name: "ExampleRemoteSite",
+            filename: "remoteEntry.js",
+            exposes: {
+              "./Content": "./src/App",
+            },
+            shared: {
+              ...dependencies,
+              react: {
+                singleton: true,
+                requiredVersion: dependencies["react"],
+              },
+              "react-dom": {
+                singleton: true,
+                requiredVersion: dependencies["react-dom"],
+              },
+            },
+        }),
+    ],
+    resolve: {
+        extensions: [".js", ".jsx"],
+    },
+    target: "web",
+};
