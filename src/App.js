f6aed7f8769099e8021d73286251d212c9e9c7af
mortis133 <kryst.madry@gmail.com>
2023-10-15 11:26:10 +0200

    Add mui config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import style from './App.module.css';
 import _ from 'lodash';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className={style.App}>
+      <Button variant="contained">Klik example-remote-site</Button>    
       Remote site - content changes
       <p>
         Lodash v - {_.VERSION}
